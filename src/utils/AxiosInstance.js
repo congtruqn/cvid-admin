faf26145ed2da64bf6769bee262a086e8702dc75
Nguyễn Trọng Hoàng <116530644+space-hoang@users.noreply.github.com>
2023-01-15 23:08:06 +0700

    1

--- src/utils/AxiosInstance.js
+++ src/utils/AxiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: `https://cvidpro.herokuapp.com/`,
-  baseURL: `http://localhost:3030/`,
+  baseURL: `https://cvidpro.herokuapp.com/`,
+  // baseURL: `http://localhost:3030/`,
 });
 
 axiosInstance.interceptors.request.use((config) => {
